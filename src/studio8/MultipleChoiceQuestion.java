ea4f92956f757bdbf45ce4254c44ef1df4ee6367
adams <adams@HarrisonPC.lan>
2025-04-02 14:19:34 -0500

    We need to watch the lecture video

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -3,6 +3,8 @@ package studio8;
 import support.cse131.NotYetImplementedException;
 
 public class MultipleChoiceQuestion extends Question {
+	
+	private String[] choices;
 
 	/**
 	 * Constructor
@@ -15,7 +17,8 @@ public class MultipleChoiceQuestion extends Question {
 		// Call the super class constructor, then create and set
 		// instance variables for any values that aren't handled
 		// by the base class
-		throw new NotYetImplementedException();
+		super(prompt, answer, points);
+		this.choices = choices;
 	}
 	
 	/**
@@ -23,7 +26,10 @@ public class MultipleChoiceQuestion extends Question {
 	 * the choices present for the question.
 	 */
 	public void displayPrompt() {
-		throw new NotYetImplementedException();
+		super.displayPrompt();
+		for (int i = 0; i < this.choices.length; i++) {
+			System.out.println(i + 1 + ". " + this.choices[i]);
+		}
 	}
 	
 	/**
@@ -31,7 +37,7 @@ public class MultipleChoiceQuestion extends Question {
 	 * @return String[] of choices
 	 */
 	public String[] getChoices() {
-		throw new NotYetImplementedException();
+		return this.choices;
 	}
 	
 	public static void main(String[] args) {
